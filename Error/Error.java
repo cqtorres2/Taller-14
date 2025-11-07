123334934c5abe8eb7107d2f959782a9fbbd27ee
34rl0s <carlosjosedeltorresrio@gmail.com>
2025-11-07 11:30:05 -0500

    1.3

---
+++ Error/Error.java
@@ -0,0 +1,28 @@
+/*package Error;
+
+public class Error {
+    void mal1(){
+        System.out.println("24");
+    }
+}
+class Error3 extends Error{
+    void mal2(){
+        System.out.println("45");
+    }
+    
+}
+class Error2 extends Error{
+    @Override
+    void mal1(){
+        System.out.println("24");
+    }
+    
+}
+class Main extends Error{
+    public static void main(String[] args) {
+        Error error = new Error();
+        error.Mal2();
+
+    }
+    
+}*/
